8a85985dce548263c1db5eb7fcfbc7bf83eaa979
Christian Storch <ccstorch@gmail.com>
2016-05-01 00:53:18 +0100

    first commit

---
+++ react-form-helpers.js
@@ -0,0 +1,39 @@
+import _ from 'lodash';
+
+function handleInputChange(object, fieldName, event) {
+  _.set(object, fieldName, event.target.value);
+}
+
+function handleInputChangeAndUpdateComponent(object, fieldName, event) {
+  handleInputChange(object, fieldName, event);
+  this.forceUpdate();
+}
+
+function handleBooleanInputChange(object, fieldName, event) {
+  const oldValue = _.result(object, fieldName);
+  let newValue = parseStringToBoolen(event.target.value);
+  if(oldValue === newValue) newValue = !newValue;
+  _.set(object, fieldName, newValue);
+}
+
+function parseStringToBoolen(stringValue) {
+  if(stringValue === 'true') return true;
+  return false;
+}
+
+function handleBooleanInputChangeAndUpdateComponent(object, fieldName, event) {
+  handleBooleanInputChange(object, fieldName, event);
+  this.forceUpdate();
+}
+
+function focusInputByRef(component, refName = 'focus') {
+  !!component.refs[refName] && component.refs[refName].focus();
+}
+
+export {
+  handleInputChange,
+  handleInputChangeAndUpdateComponent,
+  handleBooleanInputChange,
+  handleBooleanInputChangeAndUpdateComponent,
+  focusInputByRef
+};
