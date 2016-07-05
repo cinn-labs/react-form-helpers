85a664637edcd0ad0c53506a189f351eb1d05108
Christian Storch <ccstorch@gmail.com>
2016-07-05 14:41:18 +0100

    1.1.2

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'cinn:react-form-helpers',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Simple react form helpers to facilitate the comunication between the component and the input data',
   git: 'https://github.com/cinn-labs/react-form-helpers',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.4');
   api.use('ecmascript');
   api.mainModule('react-form-helpers.js');
 });
